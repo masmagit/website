9d54981699f0c28db4b464d74f406cbd759c1c22
masmagit <masma.business@gmail.com>
2021-05-07 20:26:03 +0200

    added default secret key for the build stage

--- website/settings/base.py
+++ website/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 import os
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 
 # Application definition
