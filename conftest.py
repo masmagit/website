b4bbee70da90af5382d934c246ae9736ab93ed9a
masmagit <masma.web@gmail.com>
2021-05-20 11:56:06 +0200

    contact post test added

--- conftest.py
+++ conftest.py
@@ -8,3 +8,6 @@ def pytest_sessionstart(session):
 @pytest.fixture(autouse=True)
 def env_setup(monkeypatch):
     monkeypatch.setenv('EMAIL_BACKEND', 'django.core.mail.backends.locmem.EmailBackend')
+    # use reCaptcha open testing keys
+    monkeypatch.setenv('RECAPTCHA_SITE_KEY', '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI')
+    monkeypatch.setenv('RECAPTCHA_SECRET_KEY', '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe')
--- main/tests/test-views.py
+++ main/tests/test-views.py
@@ -1,17 +1,27 @@
 import pytest
 from django.urls import reverse
 from django.conf import settings
+from django.core import mail
 # pytest main/tests/test-views.py
 
 def test_settings_loaded():
     a = settings.ROOT_URLCONF
     assert a == "website.urls"
 
-#https://docs.djangoproject.com/en/dev/topics/testing/tools/#topics-testing-email
 def test_view_contact_get(client):
     url = reverse("main:contact")
     response = client.get(url)
     assert response.status_code == 200
 
 def test_view_contact_post(client):
-    pass
+    data = {
+        'name' : 'Test name',
+    	'email' : 'from@example.com',
+        'subject' : 'test email',
+        'message' : 'Hello test'
+    }
+    url = reverse("main:contact")
+    response = client.post(url, data)
+    assert response.status_code == 302
+    assert len(mail.outbox) == 1
+    assert mail.outbox[0].body.find('Email: from@example.com') > 0
