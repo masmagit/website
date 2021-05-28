a7085b7c587da465f36c1b283c2a1c73887155d7
masmagit <masma.web@gmail.com>
2021-05-28 15:57:31 +0200

    About, content and admin, tiny

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Content(models.Model):
+    page = models.CharField(max_length=50, blank=True)
+    param1 = models.CharField(max_length=250, blank=True)
+    param2 = models.JSONField(blank=True, null=True)
+    content = models.TextField(blank=True)
--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
@@ -5,4 +6,6 @@ app_name = "main"
 urlpatterns = [
     path('', views.index, name="index"),
     path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+    path('tiny/', views.tiny),
 ]
